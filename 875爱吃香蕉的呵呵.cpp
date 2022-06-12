b288183299ad699bd7a7b550546c33b66ecdd9c0
2901905342 <44011411+2901905342@users.noreply.github.com>
2022-06-12 20:20:17 +0800

    Add files via upload

---
+++ 875爱吃香蕉的呵呵.cpp
@@ -0,0 +1,55 @@
+﻿//珂珂喜欢吃香蕉。这里有 n 堆香蕉，第 i 堆中有 piles[i] 根香蕉。
+//警卫已经离开了，将在 h 小时后回来。
+//珂珂可以决定她吃香蕉的速度 k （单位：根 / 小时）。
+//每个小时，她将会选择一堆香蕉，从中吃掉 k 根。如果这堆香蕉少于 k 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。  
+//珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+//返回她可以在 h 小时内吃掉所有香蕉的最小速度 k（k 为整数）。
+
+//如果珂珂在 hh 小时内吃掉所有香蕉的最小速度是每小时 kk 个香蕉，
+//则当吃香蕉的速度大于每小时 kk 个香蕉时一定可以在 hh 小时内吃掉所有香蕉，
+//当吃香蕉的速度小于每小时 kk 个香蕉时一定不能在 hh 小时内吃掉所有香蕉。
+//由于吃香蕉的速度和是否可以在规定时间内吃掉所有香蕉之间存在单调性，
+//因此可以使用二分查找的方法得到最小速度 kk。
+
+//1 <= piles.length <= 104
+//piles.length <= h <= 109
+//1 <= piles[i] <= 109
+
+//最难想到的是由于每小时都要吃香蕉，即每小时至少吃 1个香蕉，因此二分查找的下界是 1；
+
+#include "myhead.h"
+class Solution {
+public:
+    bool  minnum(vector<int>& piles, int target, int h) {
+        for (int i = 0; i < piles.size(); i++) {
+            if (piles[i] <= target) h--;
+            else {
+                if (piles[i] % target == 0) h -= piles[i] / target;
+                else  h -= piles[i] / target + 1;
+            }
+            if (h < 0) return false;
+        }
+        return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        // sort(piles.begin(),piles.end());
+        int left = 1;
+        int right = piles[0];
+        int mid;
+        for (int i = 0; i < piles.size(); i++) {
+            right = max(right, piles[i]);
+        }
+
+        mid = (right - left) / 2 + left;
+        while (left < right) {
+            if (minnum(piles, mid, h)) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+            mid = (right - left) / 2 + left;
+        }
+        return right;
+    }
+};
---
+++ 890.查找和替换模式.cpp
@@ -0,0 +1,41 @@
+﻿//你有一个单词列表 words 和一个模式  pattern，你想知道 words 中的哪些单词与模式匹配。
+//如果存在字母的排列 p ，使得将模式中的每个字母 x 替换为 p(x) 之后，我们就得到了所需的单词，那么单词与模式是匹配的。
+//（回想一下，字母的排列是从字母到字母的双射：每个字母映射到另一个字母，没有两个字母映射到同一个字母。）
+//返回 words 中与给定模式匹配的单词列表。
+//你可以按任何顺序返回答案。
+
+//思路：
+//我们可以逐个判断words 中的每个单词 word 是否与 pattern 匹配。
+//根据题意，我们需要构造从字母到字母的双射，即 word 的每个字母需要映射到 pattern 的对应字母，并且 pattern 的每个字母也需要映射到word 的对应字母。
+//我们可以编写一个函数 match(word, pattern)，仅当 word 中相同字母映射到 pattern 中的相同字母时返回 true。
+//我们可以在遍历这两个字符串的同时，用一个哈希表记录word 的每个字母 x 需要映射到 pattern 的哪个字母上，如果 x 已有映射，则需要检查对应字母是否相同。
+//如果 match(word, pattern) 和 match(pattern, word) 均为 \texttt{ true }true，则表示word 与 pattern 匹配。
+#include "myhead.h"
+
+class Solution {
+    bool match(string& word, string& pattern) {
+        unordered_map<char, char> mp;
+        for (int i = 0; i < word.length(); ++i) {
+            char x = word[i], y = pattern[i];
+            if (!mp.count(x)) {
+                mp[x] = y;
+            }
+            else if (mp[x] != y) { // word 中的同一字母必须映射到 pattern 中的同一字母上
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string& pattern) {
+        vector<string> ans;
+        for (auto& word : words) {
+            if (match(word, pattern) && match(pattern, word)) {
+                ans.emplace_back(word);
+            }
+        }
+        return ans;
+    }
+};
+

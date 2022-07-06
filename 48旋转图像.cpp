bae162f9674c82d23e901e3c42ac4b4ed7907f0c
GJK <44011411+2901905342@users.noreply.github.com>
2022-07-06 11:06:13 +0800

    Add files via upload

---
+++ 48旋转图像.cpp
@@ -0,0 +1,25 @@
+﻿//给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+//你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。
+//请不要 使用另一个矩阵来旋转图像。
+
+
+//先沿着对角线翻转，再沿着Y轴翻转
+
+#include "myhead.h"
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+            }
+        }
+
+    }
+};
